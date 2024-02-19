8bd9c5574a0553e2df6391edfaf389ebd6234475
YakupSadi <sadiyakupsadi@hotmail.com>
2024-02-19 21:32:30 +0300

    19/02/2024

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require( 'dotenv' ).config()
 
 const express = require( 'express' )
 const app     = express()
-const PORT    = 'https://yakupsadi.com/api/v1/'
+const PORT    = 'https://api.yakupsadi.com/api/v1/'
 
 
 // Database
